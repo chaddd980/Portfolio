9892af26ff8d819746c68f20c7dc18ba64088e9f
chadd williams <chadd980@hotmail.com>
2017-01-19 00:22:43 -0500

    added .env file

--- server.rb
+++ server.rb
@@ -1,5 +1,6 @@
 require "sinatra"
 require 'sinatra/base'
+require "sinatra"
 require 'pony'
 
 set :bind, '0.0.0.0'
@@ -10,19 +11,20 @@ get "/" do
 end
 
 post "/email" do
-  unless params[:name] = "" || params[:email] = "" || params[:message] = ""
+  unless params[:name] == "" || params[:email] == "" || params[:message] == ""
+
     Pony.mail(
       :to => 'chadd980@gmail.com',
       :from => "#{params[:email]}",
       :subject => "New message through your portfolio from #{params[:email]}",
-      :body => "#{params[:name]} says: #{params[:message]}",
+      :body => "#{params[:name]} says: #{params[:message]}. respond using #{params[:email]}",
       :via => :smtp,
       :via_options => {
         :address => 'smtp.gmail.com',
         :port => '587',
         :enable_starttls_auto => true,
-        :user_name => 'chaddd980@gmail.com',
-        :password => 'bakura15',
+        :user_name => ENV[GMAIL_USERNAME],
+        :password => ENV[GMAIL_PASSWORD],
         :authentication => :plain,
       }
     )
