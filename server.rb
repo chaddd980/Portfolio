c093997d8552dc004e9def64534c4b7d111799c3
chadd williams <chadd980@hotmail.com>
2017-01-20 05:17:05 -0500

    testing sendgrid

--- server.rb
+++ server.rb
@@ -2,7 +2,10 @@ require "sinatra"
 require 'sinatra/base'
 require "sinatra"
 require 'pony'
-require 'mail'
+require 'sendgrid-ruby'
+include SendGrid
+
+
 
 set :bind, '0.0.0.0'
 
@@ -14,20 +17,19 @@ end
 post "/email" do
   unless params[:name] == "" || params[:email] == "" || params[:message] == ""
     Pony.mail({
-      :to => 'chadd980@hotmail.com',
-      :from => "#{params[:email]}",
+      :to => 'chadd980@gmail.com',
+      :from => "My portfolio <noreply@myapp.com>",
       :subject => "New message through your portfolio from #{params[:email]}",
       :body => "#{params[:name]} says: #{params[:message]}. respond using #{params[:email]}",
-      :via => :sendmail,
+      :via => :smtp,
       :via_options => {
-        # :address => 'smtp.gmail.com',
-        # :port => '587',
-        # :enable_starttls_auto => true,
-        # :user_name => "chaddd980@gmail.com",
-        # :password => "bakura15",
-        # :authentication => :plain,
-        # :location => '/usr/sbin/sendmail'
-        # :domain => "heroku.com"
+        :address => 'smtp.sendgrid.net',
+        :port => '587',
+        :enable_starttls_auto => true,
+        :user_name => 'apikey',
+        :password => ENV['SENDGRID_API_KEY'],
+        :authentication => :plain
+        # :domain => "myapp.com"
       }
     })
   end
