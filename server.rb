e0426e7c492ac1f4cc27bd1eac9e05b598aea3b3
chadd williams <chadd980@hotmail.com>
2017-01-20 02:54:03 -0500

    switch to sendmail for pony

--- server.rb
+++ server.rb
@@ -2,6 +2,7 @@ require "sinatra"
 require 'sinatra/base'
 require "sinatra"
 require 'pony'
+require 'mail'
 
 set :bind, '0.0.0.0'
 
@@ -12,22 +13,31 @@ end
 
 post "/email" do
   unless params[:name] == "" || params[:email] == "" || params[:message] == ""
-    Pony.mail(
-      :to => 'chadd980@gmail.com',
+    # mail = Mail.deliver do
+    #   from    'chaddd980@gmail.com'
+    #   to      'chadd980@gmail.com'
+    #   subject 'This is a test email from #{para}'
+    #   body    'hi'
+    # end
+    # mail.to_s
+
+    Pony.mail({
+      :to => 'chadd980@hotmail.com',
       :from => "#{params[:email]}",
       :subject => "New message through your portfolio from #{params[:email]}",
       :body => "#{params[:name]} says: #{params[:message]}. respond using #{params[:email]}",
-      :via => :smtp,
+      :via => :sendmail,
       :via_options => {
-        :address => 'smtp.gmail.com',
-        :port => '587',
-        :enable_starttls_auto => true,
-        :user_name => "chaddd980@gmail.com",
-        :password => "bakura15",
-        :authentication => :plain,
-        :domain => "heroku.com"
+        # :address => 'smtp.gmail.com',
+        # :port => '587',
+        # :enable_starttls_auto => true,
+        # :user_name => "chaddd980@gmail.com",
+        # :password => "bakura15",
+        # :authentication => :plain,
+        :location => '/usr/sbin/sendmail'
+        # :domain => "heroku.com"
       }
-    )
+    })
   end
   redirect "/"
 end
