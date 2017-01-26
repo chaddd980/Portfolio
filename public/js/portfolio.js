398aa25b2641f444800e56b36de0218cc3ec38c1
chadd williams <chadd980@hotmail.com>
2017-01-26 12:22:20 -0500

    added title to apps and description on hover

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -1,5 +1,43 @@
 $("document").ready(function($){
 
+  $("#twitch").hover(function() {
+    $(this).append("<div class='text-center description'><p>A Javascript Web App that uses the twitch JSON API to display the status of twitch accounts and link to them</p><p>Frameworks Used: jQuery and Bootstrap</p><p>Languages Used: Javascript, CSS and HTML</p></div>")
+  }, function() {
+    $(".description").remove("")
+  });
+
+  $("#weather").hover(function() {
+    $(this).append("<div class='text-center description'><p>A Javascript Web App that uses the openweathermap API to display the weather in the users current area</p><p>Frameworks Used: jQuery and Bootstrap</p><p>Languages Used: Javascript, CSS and HTML</p></div>")
+  }, function() {
+    $(".description").remove("")
+  });
+
+  $("#quote").hover(function() {
+    $(this).append("<div class='text-center description'><p>A Javascript Web App that uses the forismatic API to provide the user with a random quote on request along with the ability to share it on twitter</p><p>Frameworks Used: jQuery and Bootstrap</p><p>Languages Used: Javascript, CSS and HTML</p></div>")
+  }, function() {
+    $(".description").remove("")
+  });
+
+  $("#wiki").hover(function() {
+    $(this).append("<div class='text-center description'><p>A Javascript Web App that uses the Wikipedia API to allow users to search up articles based on a word or phrase, or generate a random article</p><p>Frameworks Used: jQuery and Bootstrap</p><p>Languages Used: Javascript, CSS and HTML</p></div>")
+  }, function() {
+    $(".description").remove("")
+  });
+
+  $("#toe").hover(function() {
+    $(this).append("<div class='text-center description'><p>A tic tac toe web app that is very easy to beat!</p><p>Frameworks Used: React, jQuery and Bootstrap</p><p>Languages Used: Javascript, CSS and HTML</p></div>")
+  }, function() {
+    $(".description").remove("")
+  });
+
+  $("#meetup").hover(function() {
+    $(this).append("<div class='text-center description'><p>A sinatra based web app that mimics meetup.com. Users can create and join meetups and comment on these meetups</p><p>Frameworks Used: Sinatra, PostgreSQL, jQuery and Bootstrap</p><p>Languages Used: Ruby, SQL, Javascript, CSS and HTML</p></div>")
+  }, function() {
+    $(".description").remove("")
+  });
+
+
+
   var nav = $('.navbar');
 
     $(window).scroll(function () {
