20fc1ba4e46bc69428a037c1560836b056757082
chadd williams <chadd980@hotmail.com>
2017-02-10 16:42:18 -0500

    added pomodor clock app

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -42,6 +42,12 @@ $("document").ready(function($){
     $(".description").remove("")
   });
 
+  $("#pomo").hover(function() {
+    $(this).append("<div class='text-center description'><p>A React timer app that uses the pomodoro time management technique to help users be more efficient</p><p>Frameworks/Preprocessor Used: React, jQuery and Sass</p><p>Languages Used: Javascript, CSS and HTML</p></div>")
+  }, function() {
+    $(".description").remove("")
+  });
+
   if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
     $("#twitch").append("<div class='text-center description'><p>A Javascript Web App that uses the twitch JSON API to display the status of twitch accounts and link to them</p><p>Frameworks Used: jQuery and Bootstrap</p><p>Languages Used: Javascript, CSS and HTML</p></div>")
     $("#weather").append("<div class='text-center description'><p>A Javascript Web App that uses the openweathermap API to display the weather in the users current area</p><p>Frameworks Used: jQuery and Bootstrap</p><p>Languages Used: Javascript, CSS and HTML</p></div>")
@@ -50,6 +56,8 @@ $("document").ready(function($){
     $("#toe").append("<div class='text-center description'><p>A React tic tac toe web app that is very easy to beat!</p><p>Frameworks Used: React, jQuery and Bootstrap</p><p>Languages Used: Javascript, CSS and HTML</p></div>")
     $("#meetup").append("<div class='text-center description'><p>A sinatra based web app that mimics meetup.com. Users can create and join meetups and comment on these meetups</p><p>Frameworks Used: Sinatra, PostgreSQL, jQuery and Bootstrap</p><p>Languages Used: Ruby, SQL, Javascript, CSS and HTML</p></div>")
     $("#calculator").append("<div class='text-center description'><p>A basic react based calculator web app</p><p>Frameworks/Preprocessor Used: React, jQuery, Sass and Bootstrap</p><p>Languages Used: Javascript, CSS and HTML</p></div>")
+    $("#pomo").append("<div class='text-center description'><p>A React timer app that uses the pomodoro time management technique to help users be more efficient</p><p>Frameworks/Preprocessor Used: React, jQuery and Sass</p><p>Languages Used: Javascript, CSS and HTML</p></div>")
+
 
   }
 
